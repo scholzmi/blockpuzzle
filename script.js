305bbea0dc426ce7d5ddb9bc106bfba96c1c6d09
scholzmi <scholzm@me.com>
2025-07-10 07:43:39 +0000

    gg

--- script.js
+++ script.js
@@ -665,6 +665,7 @@ document.addEventListener('DOMContentLoaded', () => {
     function getCookie(name) { const nameEQ = name + "="; const ca = document.cookie.split(';'); for (let i = 0; i < ca.length; i++) { let c = ca[i]; while (c.charAt(0) == ' ') c = c.substring(1, c.length); if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length); } return null; }
     
     function pruefeUndSpeichereRekord() {
+        stopTimer();
         let rekord = istHardMode ? rekordSchwer : rekordNormal;
         let rekordCookieName = istHardMode ? 'rekordSchwer' : 'rekordNormal';
         
@@ -682,6 +683,7 @@ document.addEventListener('DOMContentLoaded', () => {
             gameOverTitel.textContent = 'Spiel vorbei!';
             gameOverText.textContent = `Deine Punktzahl: ${punkte}`;
         }
+        gameOverContainer.classList.add('sichtbar');
         gameOverContainer.classList.remove('versteckt');
     }
     
